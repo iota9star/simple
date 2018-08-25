27c8711d4b889bf97427b38b3ad69b31c547d729
iota9star <iota9star@gmail.com>
2018-08-25 15:42:22 +0800

    Update async.kt

--- async.kt
+++ async.kt
@@ -1,22 +1,106 @@
-import kotlinx.coroutines.experimental.CoroutineStart
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.Job
+
+import android.util.Log
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.newFixedThreadPoolContext
 
+inline fun <reified T> T.logi(message: () -> String) = Log.i(T::class.simpleName, message())
 
-internal val BACKGROUND = newFixedThreadPoolContext(Runtime.getRuntime().availableProcessors() * 2, "coroutine_thread_pool")
+inline fun <reified T> T.loge(error: Throwable, message: () -> String) = Log.e(T::class.simpleName, message(), error)
 
-fun <T> bg(block: suspend () -> T): Deferred<T> = async(context = BACKGROUND, start = CoroutineStart.LAZY) { block() }
+class Async<T>(val v: T? = null, val e: Exception? = null)
 
-infix fun <T> Deferred<T>.then(block: suspend (T) -> Unit): Job {
-  return launch(context = UI) {
-    try {
-      block(this@then.await())
-    } catch (e: Exception) {
-      throw e
+fun <T> start(block: suspend () -> T): Deferred<Async<T>> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) {
+        try {
+            Async(v = block())
+        } catch (e: Exception) {
+            loge(e) { "there are some errors on start: " }
+            Async<T>(e = e)
+        }
     }
-  }
+}
+
+infix fun <T, R> Deferred<Async<T>>.then(block: suspend (T) -> R): Deferred<Async<R>> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) {
+        try {
+            val e = this@then.await().e
+            if (e != null) {
+                return@async Async<R>(e = e)
+            }
+            val v = this@then.await().v
+                    ?: return@async Async<R>(e = NullPointerException("deferred is null..."))
+            Async(v = block(v))
+        } catch (e: Exception) {
+            loge(e) { "there are some errors on then: " }
+            Async<R>(e = e)
+        }
+    }
+}
+
+infix fun <T> Deferred<Async<T>>.inUI(result: suspend (Async<T>) -> Unit): Job {
+    return launch(context = UI) {
+        try {
+            result(this@inUI.await())
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
+
+infix fun <T> Deferred<Async<T>>.inSub(result: suspend (Async<T>) -> Unit): Job {
+    return launch(context = CommonPool) {
+        try {
+            result(this@inSub.await())
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
+
+fun <A, B, R> Deferred<A>.combine(b: Deferred<B>, cr: CoroutineCombine2<A, B, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await()) }
+}
+
+fun <A, B, C, R> Deferred<A>.combine(b: Deferred<B>, c: Deferred<C>, cr: CoroutineCombine3<A, B, C, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await(), c.await()) }
+}
+
+fun <A, B, C, D, R> Deferred<A>.combine(b: Deferred<B>, c: Deferred<C>, d: Deferred<D>, cr: CoroutineCombine4<A, B, C, D, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await(), c.await(), d.await()) }
+}
+
+fun <A, B, C, D, E, R> Deferred<A>.combine(b: Deferred<B>, c: Deferred<C>, d: Deferred<D>, e: Deferred<E>, cr: CoroutineCombine5<A, B, C, D, E, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await(), c.await(), d.await(), e.await()) }
+}
+
+fun <A, B, C, D, E, F, R> Deferred<A>.combine(b: Deferred<B>, c: Deferred<C>, d: Deferred<D>, e: Deferred<E>, f: Deferred<F>, cr: CoroutineCombine6<A, B, C, D, E, F, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await(), c.await(), d.await(), e.await(), f.await()) }
+}
+
+fun <A, B, C, D, E, F, G, R> Deferred<A>.combine(b: Deferred<B>, c: Deferred<C>, d: Deferred<D>, e: Deferred<E>, f: Deferred<F>, g: Deferred<G>, cr: CoroutineCombine7<A, B, C, D, E, F, G, R>): Deferred<R> {
+    return async(context = CommonPool, start = CoroutineStart.LAZY) { cr.apply(this@combine.await(), b.await(), c.await(), d.await(), e.await(), f.await(), g.await()) }
+}
+
+interface CoroutineCombine2<A, B, R> {
+    fun apply(a: A, b: B): R
+}
+
+interface CoroutineCombine3<A, B, C, R> {
+    fun apply(a: A, b: B, c: C): R
+}
+
+interface CoroutineCombine4<A, B, C, D, R> {
+    fun apply(a: A, b: B, c: C, d: D): R
+}
+
+interface CoroutineCombine5<A, B, C, D, E, R> {
+    fun apply(a: A, b: B, c: C, d: D, e: E): R
+}
+
+interface CoroutineCombine6<A, B, C, D, E, F, R> {
+    fun apply(a: A, b: B, c: C, d: D, e: E, f: F): R
+}
+
+interface CoroutineCombine7<A, B, C, D, E, F, G, R> {
+    fun apply(a: A, b: B, c: C, d: D, e: E, f: F, g: G): R
 }
